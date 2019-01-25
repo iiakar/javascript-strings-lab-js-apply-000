7d3d6c5b94f2389269b8d958857ec577da7eca3d
iiakar <ibsakar@gmail.com>
2019-01-25 20:16:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "Hello, Neil deGrasse Tyson!";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Let's talk about space"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, Let's talk about space!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = "Let's talk about space";
